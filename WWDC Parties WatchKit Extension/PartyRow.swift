15610dda7c2407cc9e5636f5346dd0dbda20694c
Geoff Pado <geoff@pado.name>
2015-02-17 19:06:25 -0600

    Remove watch app.

--- WWDC Parties WatchKit Extension/PartyRow.swift
+++
@@ -1,8 +0,0 @@
-//  Created by Geoff Pado on 1/23/15.
-//  Copyright (c) 2015 Cocoatype, LLC. All rights reserved.
-
-import WatchKit
-
-class PartyRow: NSObject {
-   @IBOutlet weak var partyNameLabel: WKInterfaceLabel!
-}
